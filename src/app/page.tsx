a6ca5429c75ef746ba692b7f6b6f08f81de58011
Felix Vemmer <felix.vemmer@gmail.com>
2022-10-28 10:38:58 +0200

    feat: boilerplate up

---
+++ src/app/page.tsx
@@ -0,0 +1,6 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+}
