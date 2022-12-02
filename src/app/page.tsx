bf491fb3dc6a2b8b97c99efb7734e50724f18bf7
Felix Vemmer <felix.vemmer@gmail.com>
2022-12-02 11:53:08 +0000

    feat: takes out website stats

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <>
       <Hero />
-      <WebsiteStats />
+      {/* <WebsiteStats /> */}
       <Skills />
       <CompanyCloud />
       <Calendar />
