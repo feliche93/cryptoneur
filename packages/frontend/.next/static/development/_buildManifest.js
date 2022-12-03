2167c076cb644fbe6a2de42997f2a2c6a3592e89
Felix Vemmer <felix.vemmer@gmail.com>
2022-12-03 10:54:52 +0000

    feat: restructures repo

---
+++ packages/frontend/.next/static/development/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/gas-fees-calculator":["static\u002Fchunks\u002Fpages\u002Fgas-fees-calculator.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fgas-fees-calculator"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
