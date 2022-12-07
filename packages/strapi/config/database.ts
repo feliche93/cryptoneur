35f6f59da1aaa68bc8f88802204b2840815de7c4
feliche93 <felix.vemmer@gmail.com>
2022-12-07 14:10:03 +0100

    feat: adds docker and db config

--- packages/strapi/config/database.ts
+++ packages/strapi/config/database.ts
@@ -1,15 +1,13 @@
 export default ({ env }) => ({
   connection: {
-    client: 'postgres',
+    client: env("DATABASE_CLIENT", "postgres"),
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'strapi'),
-      user: env('DATABASE_USERNAME', 'felix.vemmer'),
-      password: env('DATABASE_PASSWORD', 'strapi'),
-      ssl: {
-        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
-      }
+      host: env('PGHOST'),
+      port: env.int('PGPORT'),
+      database: env('PGDATABASE'),
+      user: env('PGUSER'),
+      password: env('PGPASSWORD'),
     },
+    debug: false,
   },
 });
