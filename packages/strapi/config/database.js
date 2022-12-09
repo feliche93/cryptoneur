eeab1acc25cc32bdde65b630f110c6693e12d3c5
feliche93 <felix.vemmer@gmail.com>
2022-12-09 09:28:23 +0100

    feat: first push

--- packages/strapi/config/database.js
+++ packages/strapi/config/database.js
@@ -1,11 +1,13 @@
-const path = require('path');
-
 module.exports = ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: env("DATABASE_CLIENT", "postgres"),
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      host: env('PGHOST'),
+      port: env.int('PGPORT'),
+      database: env('PGDATABASE'),
+      user: env('PGUSER'),
+      password: env('PGPASSWORD'),
     },
-    useNullAsDefault: true,
+    debug: false,
   },
 });
