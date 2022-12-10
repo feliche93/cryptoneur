e4a48965d1e42b930c0b2fe1a5cfb52745dae7b9
Evrard t'Serstevens <evrardts@gmail.com>
2022-12-10 13:55:38 +0100

    aal

---
+++ packages/frontend/src/app/advent-calendar/page.tsx
@@ -0,0 +1,17 @@
+export default function Home() {
+  return (
+    <div className="relative px-4 pt-16 pb-20 sm:px-6 lg:px-8 lg:pt-10 lg:pb-28">
+      <div>
+        <div className="card w-96 max-w-lg bg-base-100 shadow-xl">
+          <div className="card-body">
+            <h2 className="card-title">Card title!</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div className="card-actions justify-end">
+              <button className="btn-primary btn">Buy Now</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
