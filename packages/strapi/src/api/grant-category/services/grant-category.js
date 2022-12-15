aa3e5b0196e6c76660566bba3a83e1c8ee3b8cfd
feliche93 <felix.vemmer@gmail.com>
2022-12-15 10:53:32 +0100

    feat: builds new relationships for models

---
+++ packages/strapi/src/api/grant-category/services/grant-category.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * grant-category service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::grant-category.grant-category');
---
+++ packages/strapi/src/api/grant-use-case/services/grant-use-case.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * grant-use-case service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::grant-use-case.grant-use-case');
