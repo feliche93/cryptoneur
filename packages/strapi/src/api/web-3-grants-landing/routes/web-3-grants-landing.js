3fb1a76dba5abb64d61e9b78545af55c7145d3a2
feliche93 <felix.vemmer@gmail.com>
2022-12-28 16:40:00 +0100

    feat: version update and SEO

---
+++ packages/strapi/src/api/web-3-grants-landing/routes/web-3-grants-landing.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * web-3-grants-landing router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::web-3-grants-landing.web-3-grants-landing');
---
+++ packages/strapi/src/api/web-3-grants-landing/services/web-3-grants-landing.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * web-3-grants-landing service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::web-3-grants-landing.web-3-grants-landing');
