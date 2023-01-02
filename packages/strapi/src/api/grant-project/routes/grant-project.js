1bc26b2e1ee3ee1d2b665c7cedf79201283510dd
feliche93 <felix.vemmer@gmail.com>
2023-01-02 18:32:53 +0100

    [CRY-11] Funded Projects Relationship Fixes #20

---
+++ packages/strapi/src/api/grant-project/routes/grant-project.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * grant-project router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::grant-project.grant-project');
