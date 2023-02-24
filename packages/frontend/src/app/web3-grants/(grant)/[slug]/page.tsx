180442c0b92e458012523893e0d9a5e89b6c60e4
feliche93 <felix.vemmer@gmail.com>
2023-02-24 18:50:00 +0100

    feat: adds seo

--- packages/frontend/src/app/web3-grants/(grant)/[slug]/page.tsx
+++ packages/frontend/src/app/web3-grants/(grant)/[slug]/page.tsx
@@ -2,108 +2,49 @@ export const revalidate = 60
 
 import { GrantInfoCard } from '@components/web3-grants/grant/GrantInfoCard'
 import { Header } from '@components/web3-grants/grant/Header'
-import { CheckIcon, HandThumbUpIcon, UserIcon } from '@heroicons/react/20/solid'
-import { QuestionMarkCircleIcon } from '@heroicons/react/24/outline'
-import { NextPage } from 'next'
+import { createServerClient } from '@utils/supabase-server'
+import { Metadata, NextPage } from 'next'
 import { NextParsedUrlQuery } from 'next/dist/server/request-meta'
-import Link from 'next/link'
 
-const Web3GrantPage: NextPage<NextParsedUrlQuery> = ({ params }) => {
+interface Params {
+  params: {
+    slug: string
+  }
+}
+
+export async function generateMetadata({ params }: Params): Promise<Metadata> {
+  const supabase = createServerClient()
+
+  const { data: grant, error: grantError } = await supabase
+    .from('grants')
+    .select('*')
+    .eq('slug', params.slug)
+    .maybeSingle()
+
+  if (grantError || !grant) {
+    return {}
+  }
+
+  return {
+    title: grant.name,
+    description: grant.description,
+    openGraph: {
+      title: grant.name,
+      description: grant.description ? grant.description : '',
+      images: [
+        {
+          url: grant.logo,
+          width: 800,
+          height: 600,
+          alt: grant.name,
+        },
+      ],
+    },
+  }
+}
+
+const Web3GrantPage = ({ params }: Params) => {
   const { slug } = params
-
-  const user = {
-    name: 'Whitney Francis',
-    email: 'whitney@example.com',
-    imageUrl:
-      'https://images.unsplash.com/photo-1517365830460-955ce3ccd263?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=256&h=256&q=80',
-  }
-  const navigation = [
-    { name: 'Dashboard', href: '#' },
-    { name: 'Jobs', href: '#' },
-    { name: 'Applicants', href: '#' },
-    { name: 'Company', href: '#' },
-  ]
-  const breadcrumbs = [
-    { name: 'Jobs', href: '#', current: false },
-    { name: 'Front End Developer', href: '#', current: false },
-    { name: 'Applicants', href: '#', current: true },
-  ]
-  const userNavigation = [
-    { name: 'Your Profile', href: '#' },
-    { name: 'Settings', href: '#' },
-    { name: 'Sign out', href: '#' },
-  ]
-
-  const eventTypes = {
-    applied: { icon: UserIcon, bgColorClass: 'bg-gray-400' },
-    advanced: { icon: HandThumbUpIcon, bgColorClass: 'bg-blue-500' },
-    completed: { icon: CheckIcon, bgColorClass: 'bg-green-500' },
-  }
-  const timeline = [
-    {
-      id: 1,
-      type: eventTypes.applied,
-      content: 'Applied to',
-      target: 'Front End Developer',
-      date: 'Sep 20',
-      datetime: '2020-09-20',
-    },
-    {
-      id: 2,
-      type: eventTypes.advanced,
-      content: 'Advanced to phone screening by',
-      target: 'Bethany Blake',
-      date: 'Sep 22',
-      datetime: '2020-09-22',
-    },
-    {
-      id: 3,
-      type: eventTypes.completed,
-      content: 'Completed phone screening with',
-      target: 'Martha Gardner',
-      date: 'Sep 28',
-      datetime: '2020-09-28',
-    },
-    {
-      id: 4,
-      type: eventTypes.advanced,
-      content: 'Advanced to interview by',
-      target: 'Bethany Blake',
-      date: 'Sep 30',
-      datetime: '2020-09-30',
-    },
-    {
-      id: 5,
-      type: eventTypes.completed,
-      content: 'Completed interview with',
-      target: 'Katherine Snyder',
-      date: 'Oct 4',
-      datetime: '2020-10-04',
-    },
-  ]
-  const comments = [
-    {
-      id: 1,
-      name: 'Leslie Alexander',
-      date: '4d ago',
-      imageId: '1494790108377-be9c29b29330',
-      body: 'Ducimus quas delectus ad maxime totam doloribus reiciendis ex. Tempore dolorem maiores. Similique voluptatibus tempore non ut.',
-    },
-    {
-      id: 2,
-      name: 'Michael Foster',
-      date: '4d ago',
-      imageId: '1519244703995-f4e0f30006d5',
-      body: 'Et ut autem. Voluptatem eum dolores sint necessitatibus quos. Quis eum qui dolorem accusantium voluptas voluptatem ipsum. Quo facere iusto quia accusamus veniam id explicabo et aut.',
-    },
-    {
-      id: 3,
-      name: 'Dries Vincent',
-      date: '4d ago',
-      imageId: '1506794778202-cad84cf45f1d',
-      body: 'Expedita consequatur sit ea voluptas quo ipsam recusandae. Ab sint et voluptatem repudiandae voluptatem et eveniet. Nihil quas consequatur autem. Perferendis rerum et.',
-    },
-  ]
 
   return (
     <>
--- packages/frontend/src/app/web3-grants/page.tsx
+++ packages/frontend/src/app/web3-grants/page.tsx
@@ -4,6 +4,45 @@ import { FoundersNote } from '@components/web3-grants/FoundersNote'
 import { Hero } from '@components/web3-grants/Hero'
 import { SecondCta } from '@components/web3-grants/SecondCta'
 import { Testimonials } from '@components/web3-grants/Testimonials'
+import { Metadata } from 'next'
+import image1 from '@/../public/og/web-3-grants/image1.jpg'
+import image2 from '@/../public/og/web-3-grants/image2.jpg'
+import image3 from '@/../public/og/web-3-grants/image3.jpg'
+
+export const metadata: Metadata = {
+  title: 'Web3 Grants',
+  description:
+    'Find the perfect funding opportunity for your web3 project with our comprehensive and constantly updated grant database. Advanced filtering options and data-driven insights increase your chances of success. Join our growing community and contribute to the database.',
+  keywords:
+    'web3, grants, funding, crypto, blockchain, ethereum, defi, dapp, dapps, web3 grants, grants',
+  openGraph: {
+    title: 'Web3 Grant Platform - Find Funding for Your Project',
+    description:
+      'Find the perfect funding opportunity for your web3 project with our comprehensive and constantly updated grant database. Advanced filtering options and data-driven insights increase your chances of success. Join our growing community and contribute to the database.',
+    url: 'https://cryptoneur.xyz/web3-grants',
+    type: 'website',
+    images: [
+      {
+        url: `${process.env.NEXT_PUBLIC_URL}${image1.src}`,
+        width: image1.width,
+        height: image3.height,
+        alt: 'Web3 Grant Platform - Find Funding for Your Project',
+      },
+      {
+        url: `${process.env.NEXT_PUBLIC_URL}${image2.src}`,
+        width: image2.width,
+        height: image2.height,
+        alt: 'Web3 Grant Platform - Find Funding for Your Project',
+      },
+      {
+        url: `${process.env.NEXT_PUBLIC_URL}${image3.src}`,
+        width: image3.width,
+        height: image3.height,
+        alt: 'Web3 Grant Platform - Find Funding for Your Project',
+      },
+    ],
+  },
+}
 
 export default function Web3Grants() {
   return (
