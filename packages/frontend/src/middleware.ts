346c9e41f277416147e3de2f247129b8b79349d7
feliche93 <felix.vemmer@gmail.com>
2023-03-25 15:58:58 +0100

    fix: middleware

--- packages/frontend/src/middleware.ts
+++ packages/frontend/src/middleware.ts
@@ -5,35 +5,28 @@ import { match as matchLocale } from '@formatjs/intl-localematcher'
 import Negotiator from 'negotiator'
 import { i18n } from '../i18n-config'
 
-const languageCodeToTagMap: Record<string, string> = {
-  en: 'en-US',
-  de: 'de-DE',
-  // Add more mappings here as needed
-};
-
 function getLocale(request: NextRequest): string | undefined {
   // Negotiator expects plain object so we need to transform headers
-  const negotiatorHeaders: Record<string, string> = {};
-  request.headers.forEach((value, key) => (negotiatorHeaders[key] = value));
+  const negotiatorHeaders: Record<string, string> = {}
+  request.headers.forEach((value, key) => (negotiatorHeaders[key] = value))
 
   // Use negotiator and intl-localematcher to get best locale
-  let languages = new Negotiator({ headers: negotiatorHeaders }).languages();
+  let languages = new Negotiator({ headers: negotiatorHeaders }).languages()
+  const locales: string[] = i18n.locales.slice()
 
-  // Filter out malformed locales and map language codes to BCP 47 language tags
-  languages = languages
-    .filter((lang) => /^[a-zA-Z]{2,}-?[a-zA-Z]*$/.test(lang))
-    .map((lang) => languageCodeToTagMap[lang] || lang);
-
-  // @ts-ignore locales are readonly
-  const locales: string[] = i18n.locales.map(
-    (locale) => languageCodeToTagMap[locale] || locale
-  );
-  return matchLocale(languages, locales, languageCodeToTagMap[i18n.defaultLocale]);
+  return matchLocale(languages, locales, i18n.defaultLocale)
 }
 
 // this middleware refreshes the user's session and must be run
 // for any Server Component route that uses `createServerComponentSupabaseClient`
 export async function middleware(req: NextRequest) {
+
+  const res = NextResponse.next()
+  const supabase = createMiddlewareSupabaseClient({ req, res })
+
+  const {
+    data: { session },
+  } = await supabase.auth.getSession()
 
   // Skip next internal and image requests
   if (
@@ -48,27 +41,16 @@ export async function middleware(req: NextRequest) {
 
   // Check if there is any supported locale in the pathname
   const pathnameIsMissingLocale = i18n.locales.every(
-    (locale) => !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`
+    locale => !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`
   )
 
-  // Redirect if there is no locale
+  // Let's redirect if there is no locale
   if (pathnameIsMissingLocale) {
     const locale = getLocale(req)
 
-    // e.g. incoming request is /products
-    // The new URL is now /en-US/products
     return NextResponse.redirect(new URL(`/${locale}/${pathname}`, req.url))
   }
 
-  const res = NextResponse.next()
-  const supabase = createMiddlewareSupabaseClient({ req, res })
-
-  const {
-    data: { session },
-  } = await supabase.auth.getSession()
-
-  // console.log({ session })
-  // console.log({ pathname: req.nextUrl.pathname })
 
   if (session && req.nextUrl.pathname.startsWith('/sign-in')) {
 
@@ -86,15 +68,6 @@ export async function middleware(req: NextRequest) {
     redirectUrl.searchParams.set(`redirectedFrom`, req.nextUrl.pathname)
     return NextResponse.redirect(redirectUrl)
   }
-
-  // if (!session && req.nextUrl.pathname.startsWith('/web3-grants')) {
-  //   console.log('redirecting to sign-in')
-  //   // Auth condition not met, redirect to home page.
-  //   const redirectUrl = req.nextUrl.clone()
-  //   redirectUrl.pathname = '/sign-in'
-  //   redirectUrl.searchParams.set(`redirectedFrom`, req.nextUrl.pathname)
-  //   return NextResponse.redirect(redirectUrl)
-  // }
 
   return res
 }
