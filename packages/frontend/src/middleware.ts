42ece6a8cd4aa41e4722e5468ff285b98f00dd63
feliche93 <felix.vemmer@gmail.com>
2023-03-27 21:24:10 +0200

    feat: latest

--- packages/frontend/src/middleware.ts
+++ packages/frontend/src/middleware.ts
@@ -7,13 +7,14 @@ import { i18n } from '../i18n-config'
 
 function getLocale(request: NextRequest): string | undefined {
   // Negotiator expects plain object so we need to transform headers
-  const negotiatorHeaders: Record<string, string> = {};
-  request.headers.forEach((value, key) => (negotiatorHeaders[key] = value));
+  const negotiatorHeaders: Record<string, string> = {}
+  request.headers.forEach((value, key) => (negotiatorHeaders[key] = value))
 
-  // Use Negotiator to get the best locale
-  const negotiator = new Negotiator({ headers: negotiatorHeaders });
-  // @ts-ignore
-  return negotiator.language(i18n.locales) || i18n.defaultLocale;
+  // Use negotiator and intl-localematcher to get best locale
+  let languages = new Negotiator({ headers: negotiatorHeaders }).languages()
+  const locales: string[] = i18n.locales.slice()
+
+  return matchLocale(languages, locales, i18n.defaultLocale)
 }
 
 // this middleware refreshes the user's session and must be run
@@ -75,6 +76,7 @@ export const config = {
   matcher: [
     '/sign-in',
     '/register',
-    '/((?!api|_next/static|_next/image|favicon.ico|gas-fees-calculator|web-3-grants).*)'
+    '/((?!api|_next/static|_next/image|favicon.ico).*)',
+    '/((?!gas-fees-calculator|blog).*)'
   ],
 }
