29ac5677e18c1208d2dc8d34e7fca12f3aed4a97
feliche93 <felix.vemmer@gmail.com>
2023-04-01 20:37:51 +0200

    Add function to generate page metadata dynamically for SEO purposes

--- packages/frontend/src/app/[lang]/page.tsx
+++ packages/frontend/src/app/[lang]/page.tsx
@@ -5,6 +5,23 @@ import { notFound } from 'next/navigation'
 
 export const dynamic = 'error'
 
+export const generateMetadata = async ({ params }: { params: { slug: string; lang: string } }) => {
+  const { slug, lang } = params
+  const pageData = await fetchPageData(slug, lang)
+
+  const seo = pageData?.seo
+
+  if (!seo || typeof seo !== 'object' || !seo?.id) {
+    return null
+  }
+
+  const metaData = await getMetaData(seo?.id, lang)
+
+  // console.log(metaData?.openGraph?.images)
+
+  return metaData
+}
+
 const HomePage = async ({ params }: { params: { slug: string; lang: string } }) => {
   const { slug, lang } = params
   preload(slug, lang)
