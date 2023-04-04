444243578c3cd342195c22a818a96fd58780aea3
feliche93 <felix.vemmer@gmail.com>
2023-04-04 16:36:54 +0200

    Remove unused imports and simplify code in web3-grants page component

--- packages/frontend/src/app/[lang]/web3-grants/[slug]/page.tsx
+++ packages/frontend/src/app/[lang]/web3-grants/[slug]/page.tsx
@@ -1,9 +1,6 @@
-import { RenderBlock } from '@components/render-block'
 import { GrantInfoCard } from '@components/web3-grants/grant/grant-info-card'
 import { Header } from '@components/web3-grants/grant/header'
-import directus, { fetchPageData, preload } from '@lib/directus'
-import { BlockType } from '@lib/directus.types'
-import { notFound } from 'next/navigation'
+import directus from '@lib/directus'
 import { z } from 'zod'
 
 const grantDataSchema = z.array(z.object({ slug: z.string(), id: z.number() }))
