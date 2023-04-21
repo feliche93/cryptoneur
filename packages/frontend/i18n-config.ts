ccc0d61154e67ed5c20b7383c8e38b4a966a0f8d
feliche93 <felix.vemmer@gmail.com>
2023-04-21 18:46:47 +0200

    feat: add all locales

--- packages/frontend/i18n-config.ts
+++ packages/frontend/i18n-config.ts
@@ -1,6 +1,14 @@
 export const i18n = {
     defaultLocale: 'en',
-    locales: ['en', 'zh', 'de'],
+    locales: [
+        "de",
+        "es",
+        "fr",
+        "it",
+        "pt",
+        "ru",
+        "zh",
+    ],
 } as const
 
 export type Locale = typeof i18n['locales'][number]
