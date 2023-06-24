00a782d808cf3026dcb5f64609aa5d521a68ed45
feliche93 <felix.vemmer@gmail.com>
2023-06-24 14:12:03 +0200

    feat: backlink home page

--- packages/frontend/src/components/layout/Footer.tsx
+++ packages/frontend/src/components/layout/Footer.tsx
@@ -1,4 +1,5 @@
-import Link from "next/link";
+import { GlobeAltIcon } from '@heroicons/react/24/outline'
+import Link from 'next/link'
 
 const navigation = {
   main: [
@@ -11,8 +12,8 @@ const navigation = {
   ],
   social: [
     {
-      name: "Ethereum",
-      href: "https://etherscan.io/enslookup-search?search=cryptoneur.eth",
+      name: 'Ethereum',
+      href: 'https://etherscan.io/enslookup-search?search=cryptoneur.eth',
       icon: (props) => (
         <svg
           fill="currentColor"
@@ -27,8 +28,8 @@ const navigation = {
       ),
     },
     {
-      name: "GitHub",
-      href: "https://github.com/feliche93",
+      name: 'GitHub',
+      href: 'https://github.com/feliche93',
       icon: (props) => (
         <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
           <path
@@ -40,8 +41,8 @@ const navigation = {
       ),
     },
     {
-      name: "Twitter",
-      href: "https://twitter.com/cryptoneur_eth",
+      name: 'Twitter',
+      href: 'https://twitter.com/cryptoneur_eth',
       icon: (props) => (
         <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
           <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
@@ -49,38 +50,28 @@ const navigation = {
       ),
     },
     {
-      name: "Discord",
-      href: "https://discordapp.com/users/feliche#3785",
+      name: 'Discord',
+      href: 'https://discordapp.com/users/feliche#3785',
       icon: (props) => (
-        <svg
-          fill="currentColor"
-          viewBox="0 0 24 24"
-          {...props}
-          xmlns="http://www.w3.org/2000/svg"
-        >
+        <svg fill="currentColor" viewBox="0 0 24 24" {...props} xmlns="http://www.w3.org/2000/svg">
           {/* <title>Discord</title> */}
           <path d="M20.317 4.3698a19.7913 19.7913 0 00-4.8851-1.5152.0741.0741 0 00-.0785.0371c-.211.3753-.4447.8648-.6083 1.2495-1.8447-.2762-3.68-.2762-5.4868 0-.1636-.3933-.4058-.8742-.6177-1.2495a.077.077 0 00-.0785-.037 19.7363 19.7363 0 00-4.8852 1.515.0699.0699 0 00-.0321.0277C.5334 9.0458-.319 13.5799.0992 18.0578a.0824.0824 0 00.0312.0561c2.0528 1.5076 4.0413 2.4228 5.9929 3.0294a.0777.0777 0 00.0842-.0276c.4616-.6304.8731-1.2952 1.226-1.9942a.076.076 0 00-.0416-.1057c-.6528-.2476-1.2743-.5495-1.8722-.8923a.077.077 0 01-.0076-.1277c.1258-.0943.2517-.1923.3718-.2914a.0743.0743 0 01.0776-.0105c3.9278 1.7933 8.18 1.7933 12.0614 0a.0739.0739 0 01.0785.0095c.1202.099.246.1981.3728.2924a.077.077 0 01-.0066.1276 12.2986 12.2986 0 01-1.873.8914.0766.0766 0 00-.0407.1067c.3604.698.7719 1.3628 1.225 1.9932a.076.076 0 00.0842.0286c1.961-.6067 3.9495-1.5219 6.0023-3.0294a.077.077 0 00.0313-.0552c.5004-5.177-.8382-9.6739-3.5485-13.6604a.061.061 0 00-.0312-.0286zM8.02 15.3312c-1.1825 0-2.1569-1.0857-2.1569-2.419 0-1.3332.9555-2.4189 2.157-2.4189 1.2108 0 2.1757 1.0952 2.1568 2.419 0 1.3332-.9555 2.4189-2.1569 2.4189zm7.9748 0c-1.1825 0-2.1569-1.0857-2.1569-2.419 0-1.3332.9554-2.4189 2.1569-2.4189 1.2108 0 2.1757 1.0952 2.1568 2.419 0 1.3332-.946 2.4189-2.1568 2.4189Z" />
         </svg>
       ),
     },
     {
-      name: "LinkedIn",
-      href: "https://www.linkedin.com/in/felix-vemmer/",
+      name: 'LinkedIn',
+      href: 'https://www.linkedin.com/in/felix-vemmer/',
       icon: (props) => (
-        <svg
-          fill="currentColor"
-          viewBox="0 0 24 24"
-          {...props}
-          xmlns="http://www.w3.org/2000/svg"
-        >
+        <svg fill="currentColor" viewBox="0 0 24 24" {...props} xmlns="http://www.w3.org/2000/svg">
           {/* <title>LinkedIn</title> */}
           <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z" />
         </svg>
       ),
     },
     {
-      name: "Instagram",
-      href: "https://www.instagram.com/feliche93/",
+      name: 'Instagram',
+      href: 'https://www.instagram.com/feliche93/',
       icon: (props) => (
         <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
           <path
@@ -91,13 +82,18 @@ const navigation = {
         </svg>
       ),
     },
+    {
+      name: 'Website',
+      href: 'https://www.felixvemmer.com/',
+      icon: (props) => <GlobeAltIcon className="h-6 w-6" aria-hidden="true" />,
+    },
   ],
-};
+}
 
 export default function Footer() {
   return (
     <footer className="bg-base-200">
-      <div className="max-w-7xl mx-auto py-12 px-4 overflow-hidden sm:px-6 lg:px-8">
+      <div className="mx-auto max-w-7xl overflow-hidden px-4 py-12 sm:px-6 lg:px-8">
         {/* <nav
           className="-mx-5 -my-2 flex flex-wrap justify-center"
           aria-label="Footer"
@@ -125,10 +121,8 @@ export default function Footer() {
             </Link>
           ))}
         </div>
-        <p className="mt-8 text-center text-base text-bae-content/80">
-          &copy; 2022 Cryptoneur.
-        </p>
+        <p className="text-bae-content/80 mt-8 text-center text-base">&copy; 2022 Cryptoneur.</p>
       </div>
     </footer>
-  );
+  )
 }
