d98ee23f00b62d77bf7ff2fdb694c14c8ea8c471
feliche93 <felix.vemmer@gmail.com>
2023-08-26 00:01:55 +0200

    feat: form card

--- packages/svelte-rewrite/src/routes/gas-fees-calculator/form-card.svelte
+++ packages/svelte-rewrite/src/routes/gas-fees-calculator/form-card.svelte
@@ -3,7 +3,7 @@
 	export let description: string;
 </script>
 
-<div class="rounded-lg bg-base-300 px-8 py-5 shadow-lg sm:p-6">
+<div class="rounded-lg bg-base-200 px-8 py-5 shadow-md sm:p-6">
 	<div class="md:grid md:grid-cols-2 md:gap-6">
 		<div class="md:col-span-1">
 			<h3 class="text-lg font-medium leading-6">{title}</h3>
