89fa0f78c05753de599e9348ab2ffd3fda26ebcc
feliche93 <felix.vemmer@gmail.com>
2024-03-19 09:52:36 +0100

    chore: fix deploy erros

--- packages/frontend/src/app/[locale]/web3-grants/page.tsx
+++ packages/frontend/src/app/[locale]/web3-grants/page.tsx
@@ -4,12 +4,12 @@ export default function Web3GrantsPage() {
   return (
     <div className="container py-12 relative">
       <PageHeader className="flex flex-col items-center">
-        <h2 className="text-lg text-center sm:text-left py-2 font-bold">We'll Back Soon</h2>
+        <h2 className="text-lg text-center sm:text-left py-2 font-bold">{`We'll Back Soon`}</h2>
         <PageHeaderHeading className="text-center sm:text-left">
           Web3 Grants Coming Soon ™
         </PageHeaderHeading>
         <PageHeaderDescription className="text-center sm:text-left">
-          We're currently working on the best resources for Web3 entrepreneurs. Stay tuned!
+          {`We're currently working on the best resources for Web3 entrepreneurs. Stay tuned!`}
         </PageHeaderDescription>
       </PageHeader>
     </div>
--- packages/frontend/src/utils/helpers.ts
+++ packages/frontend/src/utils/helpers.ts
@@ -18,7 +18,7 @@ function currencyFormatter({
 }
 
 function ordinal_suffix_of(i: number) {
-  var j = i % 10,
+  const j = i % 10,
     k = i % 100
   if (j == 1 && k != 11) {
     return i + 'st'
